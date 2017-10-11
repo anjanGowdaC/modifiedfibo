7bb99e3d4e674b818e22a4b9b328861631866e03
Your Name <dharmendra@rytangle.com>
2017-10-11 16:24:00 +0530

    func removed

--- src/android/Hello.java
+++ src/android/Hello.java
@@ -9,33 +9,16 @@ public class Hello extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
 
-        if (action.equals("greet")) {
+        if (action.equals("start")) {
 
             String name = data.getString(0);
-            String message = "***************, " + name;
+            String message = "started, " + name;
             callbackContext.success(message);
 
             return true;
 
         } else {
             
-            return false;
-
-        }
-
-            }
-
-  @Override
-    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
-
-        if(action.equals("start")){
-            String name = data.getString(0);
-            String message = "start " + name;
-            callbackContext.success(message);
-
-            return true;
-
-        } else {
             return false;
 
         }
--- www/hello.js
+++ www/hello.js
@@ -1,9 +1,7 @@
 /*global cordova, module*/
 
 module.exports = {
-    greet: function (name, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "Hello", "greet", [name]);
-    }
+    
     start: function (name, successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "Hello", "greet", [name]);
     }
